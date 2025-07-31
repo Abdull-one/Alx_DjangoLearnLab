96d9d3abd1d02f4f0e2942b2614c7e3a82f95eac
abdull-one <abdullahijangadeQ@gmail.com>
2025-07-31 16:35:41 +0100

    Implement authentication and permissions using DRF token authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,9 @@
-from rest_framework import generics
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
-
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-# ViewSet for full CRUD
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
